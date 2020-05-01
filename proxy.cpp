d127328cc02ed4c79821e0efdc3d40d98ddb4296
willis240 <willingness240@gmail.com>
2020-04-30 22:15:06 -0800

    initial commit

---
+++ proxy.cpp
@@ -0,0 +1,12 @@
+//proxy.cpp
+//William Fisher
+//Apr. 30, 2020
+
+#include<iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+}
---
+++ proxy.hpp
@@ -0,0 +1,10 @@
+//proxy.cpp
+//William Fisher
+//Apr. 30, 2020
+
+#ifndef PROXY_HPP
+#define PROXY_HPP
+
+
+
+#endif
